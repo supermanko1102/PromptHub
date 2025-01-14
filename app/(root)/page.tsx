19a273d75e3e1a31071c5767eb8446b9faf22270
Alex <caowenjieko@gmail.com>
2025-01-14 18:31:32 +0800

    feat: add search query

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,28 @@
-import { Hero } from "@/components/Hero";
+import { HeroParallax } from "@/components/HeroParallax";
+import { SanityLive } from "@/sanity/lib/live";
+import { startupQuery } from "@/sanity/lib/queries";
+import { sanityFetch } from "@/sanity/lib/live";
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query: string }>;
+}) {
+  const query = (await searchParams).query;
+  const param = { search: query || null };
+  const { data: posts } = await sanityFetch({
+    query: startupQuery,
+    params: param,
+  });
   return (
     <div className="text-3xl font-bold max-w-full">
-      <Hero />
+      <>
+        <section className="max-h-full max-w-full">
+          <HeroParallax products={posts} />
+        </section>
+
+        <SanityLive />
+      </>
     </div>
   );
 }
